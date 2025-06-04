416e3714fa510a68845c52d6f34d48ca84a2d084
Akosua Anima <akosuaanima73@gmail.com>
2025-06-04 13:24:49 +0000

    eventupdate

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,13 @@
+import eventCardImage from "../assets/event-card.png";
+
+
+export default function EventCard() {
+    return (
+    <div>
+        <img src={eventCardImage} alt="Event Card" />
+        <h1>Bestseller Book Bootcamp -write,market & Publish Your Book -lucknow</h1>
+        <h2>saturday ,march 18, 9:30PM</h2>
+        <h3>ONLINE EVENT -Attend anywhere</h3>
+    </div>
+    );
+}   
---
+++ src/pages/ViewCollege.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Nav";
+import Footer from "../components/Footer";
+
+export default function ViewCollege() {
+    return(
+     <>
+        <Navbar/>
+        <section>
+        <h1>Welcome to the ViewCollege Page</h1>
+        </section>
+         <Footer/>
+      </>
+    );
+    
+}
