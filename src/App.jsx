681a73ab756717bd5857aaaa5e2ae7bb920eb3c2
Akosua Anima <akosuaanima73@gmail.com>
2025-06-04 13:42:58 +0000

    should work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter,RouterProvider } from "react-router"
 import Register from "./pages/Register"
-import Login from "./pages/Login"
+// import Login from "./pages/Login"
 import Home from "./pages/Home"
 import ViewEvent from "./pages/Viewevent"
 import ViewCollege from "./pages/ViewCollege"
@@ -10,7 +10,7 @@ import NotFound from "./pages/NotFound"
 const eventHiveRouter = createBrowserRouter([
 { path: '/', element: <Home /> },
 { path: '/register', element: <Register /> },
-{ path: '/login', element: <Login /> },
+// { path: '/login', element: <Login /> },
 { path: '/ view-event', element: <ViewEvent/> },
 { path: '/ view-college', element: <ViewCollege/> },
 { path: '/create-event', element: <CreatEvent/> },
