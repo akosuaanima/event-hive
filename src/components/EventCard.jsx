892accc66316bca6db308c108dd68170b10a96fd
Akosua Anima <akosuaanima73@gmail.com>
2025-06-04 15:43:17 +0000

    done for today

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,10 @@
 import eventCardImage from "../assets/event-card.png";
 
 
-export default function EventCard() {
+export default function EventCard({event}) {
     return (
     <div>
-        <img src={eventCardImage} alt="Event Card" />
+        <img src={event.image} alt="Event Card" />
         <h1>Bestseller Book Bootcamp -write,market & Publish Your Book -lucknow</h1>
         <h2>saturday ,march 18, 9:30PM</h2>
         <h3>ONLINE EVENT -Attend anywhere</h3>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,79 @@
 import Navbar from "../components/Nav";
 import Footer from "../components/Footer";
-import { Search } from "lucide-react";      
+import { Search } from "lucide-react";
 import EventCard from "../components/EventCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+    //Declear state Variable in React
     const [events, setEvents] = useState([]);
 
-    return(
-      <>
-        <Navbar/>
-        <section className="bg-[url(./assets/images/image.png)] h-[596px] bg-cover w-[95%] mx-auto relative">
-             <h1 className="text-big-heading text-white w-[30%] mx-auto text-center pt-32">MADE FOR THOSE WHO DO</h1>
-             <form className="bg-navy-blue  w-[90%] mx-auto py-6 px-10 flex flex-row justify-between absolute -bottom-12 left-[5%]">
-                <div className="flex flex-col w-[30%]">
-                    <label className="text-white" htmlFor="type"> Looking for</label>
-                    <select name="type" id="type" className="bg-white p-2 rounded-md">
-                    <option selected disabled>Choose event type</option>
-                    <option value="drama">Drama</option>
-                    <option value="tech">Tech</option>
-                    </select>
-                </div>
+    //Define a function to fetch events from API
+    const getEvents = () => {
+        axios.get('https://fakestoreapi.com/products')
+            .then(response => {
+                setEvents(response.data);
+            })
+            .catch(error => {
+                console.log(error);
 
-                <div className="flex flex-col w-[30%]">
-                    <label className="text-white" htmlFor="location"> Location</label>
-                    <select name="location" id="location" className="bg-white p-2 rounded-md">
-                    <option selected disabled>Choose Location </option>
-                    <option value="accra">Accra</option>
-                    <option value="kumasi">Kumasi</option>
-                    </select>
+            })
+    }
+    //Run fetcher based on side-effects
+    useEffect(getEvents, []);
+
+    return (
+        <>
+            <Navbar />
+            <section className="bg-[url(./assets/images/image.png)] h-[596px] bg-cover w-[95%] mx-auto relative">
+                <h1 className="text-big-heading text-white w-[30%] mx-auto text-center pt-32">MADE FOR THOSE WHO DO</h1>
+                <form className="bg-navy-blue  w-[90%] mx-auto py-6 px-10 flex flex-row justify-between absolute -bottom-12 left-[5%]">
+                    <div className="flex flex-col w-[30%]">
+                        <label className="text-white" htmlFor="type"> Looking for</label>
+                        <select name="type" id="type" className="bg-white p-2 rounded-md">
+                            <option selected disabled>Choose event type</option>
+                            <option value="drama">Drama</option>
+                            <option value="tech">Tech</option>
+                        </select>
+                    </div>
+
+                    <div className="flex flex-col w-[30%]">
+                        <label className="text-white" htmlFor="location"> Location</label>
+                        <select name="location" id="location" className="bg-white p-2 rounded-md">
+                            <option selected disabled>Choose Location </option>
+                            <option value="accra">Accra</option>
+                            <option value="kumasi">Kumasi</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col w-[30%]">
+                        <label className="text-white" htmlFor="when">when</label>
+                        <input type="datetime-local" name="when" id="when" className="bg-white p-1.5 rounded-md" />
+                    </div>
+                    <div className="bg-primary text-white size-[60px] flex flex-row justify-center items-center rounded-md " >
+                        <Search />
+                    </div>
+                </form>
+            </section>
+            <section className="mt-20">
+                <div>
+                    <h1>
+                        <span className="uppercase text-2xl text-bolder mb-2">Upcoming</span>
+                        <span className="uppercase text-2xl text-blue-600 text-bolder mb-2">Events</span>
+                    </h1>
                 </div>
-                <div className="flex flex-col w-[30%]">
-                    <label className="text-white" htmlFor="when">when</label>
-                    <input type="datetime-local" name="when" id="when" className="bg-white p-1.5 rounded-md" />
-                    </div>
-                <div className="bg-primary text-white size-[60px] flex flex-row justify-center items-center rounded-md " >
-                    <Search />
-                    </div>
-             </form>
-         </section>
-                     <section className="mt-20">
-                        <div>
-                        <h1>
-                            <span className="uppercase text-2xl text-bolder mb-2">Upcoming</span>
-                            <span className="uppercase text-2xl text-blue-600 text-bolder mb-2">Events</span>
-                        </h1>
-                        </div>
-                        <div className="grid grid-cols-3 gap-5">
-                            {[1, 2, 3, 4, 5, 6].map (n => <EventCard key={n} />)}
-                        </div>
-                    </section>
-        
-        <Footer/>
+                <div className="grid grid-cols-3 gap-5">
+                    {events.map(item => {
+                        return (
+                            <EventCard
+                                key={item.id}
+                                event={item} />
+                        );
+                    })}
+                </div>
+            </section>
+
+            <Footer />
         </>
-    ); 
+    );
 }
